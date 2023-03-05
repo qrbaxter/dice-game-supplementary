0a63ea3831882ef77f425e78d1d009a498c795ea
qrbaxter <qrbaxter90@gmail.com>
2023-03-06 00:15:56 +1100

    commit
    commit

--- pages/Game.js
+++ pages/Game.js
@@ -1,16 +1,26 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import Die from '../Die';
 import { nanoid } from 'nanoid';
 
 function Game() {
     
-useEffect(()=>{
-    setButtonsDisabled(false)
-    return () => {
-            setButtonsDisabled(true)
-        }
-}, [])
-    
+   function threePairs(dice) {
+  const encounteredDice = {};
+
+  dice.forEach(die => {
+    if (encounteredDice[die]) {
+      encounteredDice[die]++;
+    } else {
+      encounteredDice[die] = 1;
+    }
+  });
+
+  const hasThreePairs = Object.values(encounteredDice).filter(cardinality => cardinality === 2).length === 3;
+  return hasThreePairs;
+}
+
+
 
   const [dice, setDice] = useState([
     { id: nanoid(), value: 0, isHeld: false },
@@ -20,13 +30,19 @@ useEffect(()=>{
     { id: nanoid(), value: 0, isHeld: false },
     { id: nanoid(), value: 0, isHeld: false },
   ]);
-
+  const allDiceHeld = dice.map(die => ({ ...die, isHeld: true }));
   const [bankedScore, setBankedScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
   const [diceSet, setDiceSet] = useState(false);
   const [buttonsActive, setButtonsActive] = useState(true);
-  const [buttonsDisabled, setButtonsDisabled] = useState(true);
+  const [newDiceBtnDisabled, setNewDiceBtnDisabled] = useState(false);
+  const [rollBtnDisabled, setRollBtnDisabled] = useState(false)
   const [busted, setBusted] = useState(false);
+  const [bankerinoBlock, setBankerinoBlock] = useState(false)
+  const [endTurnBtnDisabled, setEndTurnBtnDisabled] = useState(true)
+  const [bustCount, setBustCount]=useState(0)
+  const [endGame, setEndGame]=useState(false)
+
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
@@ -36,73 +52,140 @@ useEffect(()=>{
     }))
   }
     
-    function buttonDisable(){
-        if (buttonsDisabled){
-            
-        }
-    }
-  const bankPoints = () => {
-  let ones = 0;
-  let fives = 0;
-  let newDice = [...dice];
-  for (let i = 0; i < 6; i++) {
-    if (newDice[i].isHeld && (newDice[i].value === 1 || newDice[i].value === 5)) {
-      if (newDice[i].value === 1) {
-        ones++;
-      } else {
-        fives++;
-      }
-      newDice[i].isHeld = false;
-      newDice[i].value = 0; // Add this line to remove 1's and 5's from UI
-    }
-  }
-  setDice(newDice);
-  setBankedScore(parseInt(bankedScore) + ones * 50 + fives * 100);
-  setCurrentScore(parseInt(currentScore) + ones * 50 + fives * 100)
-  if (ones === 0 && fives === 0) {
-    setTimeout(() => {
-      setBusted(true);
-      setButtonsDisabled(true)
-      setCurrentScore(0)
-    }, 300);
-  } else {
-    setBusted(false);
-  }
-};
+
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  const bankPoints = () => {
+    let ones = 0;
+    let fives = 0;
+    
+    
+    if(!threePairs(dice)){
+
+    for (let i = 0; i < 6; i++) {
+        if (dice[i].isHeld && (dice[i].value === 1 || dice[i].value === 5)) {
+        if (dice[i].value === 1) {
+            ones++;
+        } else {
+            fives++;
+        }
+        dice[i].isHeld = false;
+        dice[i].value = 0
+        setCurrentScore((currentScore) + ones * 100 + fives * 50);
+        setRollBtnDisabled(false);
+        }
+        else if (ones === 0 && fives === 0) {
+        
+        setBusted(true);
+        setCurrentScore(0);
+        setRollBtnDisabled(false);
+        setRollBtnDisabled(true);
+        setBankerinoBlock(true)
+    
+        } 
+    
+    
+  
+  }} 
+        else if (threePairs(dice)){
+            setCurrentScore(currentScore+500)
+                setDice(oldDice => oldDice.map(die => {
+                    return {...die, isHeld: !die.isHeld, value: 0} 
+            
+                }))
+              
+                
+              
+        }
+  
+   
+    
+    
+    };
+      
+   
+    
+    
+
+        
+    const endTurn = () => {
+        
+        setBankedScore(currentScore+bankedScore)
+        setCurrentScore(0)
+        newSet()
+       
+        
+    }
     const rollDice = () => {
+  if (currentScore > 350) {
+    setEndTurnBtnDisabled(false);
+  } else {
+    setEndTurnBtnDisabled(true);
+  }
+  setRollBtnDisabled(true);
   let newDice;
-  if (bankedScore > 0) {
+  if (currentScore > 0) {
     newDice = dice.map((die) => {
-      return !die.isHeld && die.value !== 0
-        ? { ...die, value: Math.floor(Math.random() * 6) + 1 }
+      return die.value !==0
+        ? { ...die, value: Math.floor(Math.random() * 6) + 1, isHeld: die.isHeld=false} 
         : { ...die };
     });
   } else {
-    newDice = dice.map((die) => {
-      return !die.isHeld
+    newDice = !dice.map((die) => {
+      return die.value !==0
         ? { ...die, value: Math.floor(Math.random() * 6) + 1 }
         : { ...die };
     });
   }
+
   const onesAndFives = newDice.filter((die) => die.value === 1 || die.value === 5);
   if (onesAndFives.length === 0) {
-    setTimeout(() => {
+    
       setBusted(true);
-      setButtonsDisabled(true)
-      setCurrentScore(0)
-      return <div>
-      <h1 className="gameh1">YOU BUSTED!</h1>
-      </div>
-    }, 300);
+      setRollBtnDisabled(true);
+      setCurrentScore(0);
+      return (
+        <div>
+          <h1 className="gameh1">YOU BUSTED!</h1>
+        </div>
+      );
+    
   } else {
     setBusted(false);
   }
   setDice(newDice);
 };
+const resetGame = () => {
+    setBankedScore(0)
+    setCurrentScore(0)
+    localStorage.clear()
+    setBustCount(0)
+    newSet()
+}
   const newSet = () => {
-    setButtonsDisabled(false)
-  setButtonsActive(false);
+      setRollBtnDisabled(true);
+    
+    setNewDiceBtnDisabled(true);
+    setBankerinoBlock(false)
   const newDice = [];
     for (let i = 0; i < 6; i++) {
       newDice.push({ id: nanoid(), value: Math.floor(Math.random() * 6) + 1, isHeld: false });
@@ -115,22 +198,48 @@ useEffect(()=>{
   useEffect(() => {
     if (busted) {
       localStorage.setItem('bankedScore', bankedScore);
+      setRollBtnDisabled(true)
+      setNewDiceBtnDisabled(false)
+      setBankerinoBlock(true)
+      setEndTurnBtnDisabled(true)
+      setBustCount(bustCount+1)
     }
   }, [busted]);
+  useEffect(()=>{
+      if(currentScore < 350){
+          setEndTurnBtnDisabled(true)
+      }else {
+          setEndTurnBtnDisabled(false)
+      }
+  }, [currentScore])
 
   return (
     <div>
-    <div className = "theScore">
-    <p>Banked: {bankedScore}</p>
-    </div>
-      {busted ?
-        <>
-          <h1 className="gameh1">YOU BUSTED!</h1>
-        </> :
-        <>
-          <h1 className="gameh1">SCORE: {currentScore}</h1>
-        </>
-      }
+        <div className="topSection">
+            <div>
+            <p className="bankedText">Score: {bankedScore}</p>
+            <p className="bustedText">Busts: {bustCount}</p>
+            </div>
+            
+            <div>
+            {busted ?
+            <>
+            <h1 className="gameh1">YOU BUSTED!</h1>
+            </> :
+            <>
+            <h1 className="gameh1">BANKED: {currentScore}</h1>
+            </>
+        }
+            </div>
+            
+            <div>
+            <button className="endTurnBtn" onClick={endTurn} disabled={endTurnBtnDisabled}>End Turn</button>
+            <button className="resetBtn" onClick={resetGame}>Reset Game</button>
+            </div>
+            
+        </div>
+      
+<div>
       <div className="dice-container">
         {busted ? null :
           dice.map((die) => (
@@ -146,20 +255,26 @@ useEffect(()=>{
       </div>
       <div className="buttons1">
   {diceSet && (
-    <button className="rollBtn" onClick={rollDice} disabled={buttonsDisabled}
-    activation={buttonsActive}>
+    <button className="rollBtn" onClick={rollDice} disabled={rollBtnDisabled}>
       Roll Dice
     </button>
   )}
-  <button className="newDiceBtn" onClick={newSet} disabled={buttonsDisabled} activation={buttonsActive}>
+  <button className="newDiceBtn" onClick={newSet} disabled={newDiceBtnDisabled}>
     New Dice
   </button>
-  <button className="bankBtn" onClick={bankPoints}>
+  <button className="bankBtn" onClick={bankPoints} disabled={bankerinoBlock}>
     Bank Points
   </button>
 </div>
       </div>
+     </div>
   );
 }
 
 export default Game;
+
+
+
+
+
+
